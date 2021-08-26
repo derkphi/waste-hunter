e7f51b5ee0112952945e8f7547fe5e0ebc62bd13
Philipp Derksen <derksenphilipp@gmail.com>
2021-08-26 13:44:49 +0200

    Private and protected route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import Login from './components/login';
 import Home from './screens/home';
 import Calendar from './screens/calendar';
 import Reports from './screens/reports';
 import { Routes } from './constants';
-import RestrictedRoute from './components/restrictedRoute' 
-import PrivateRoute from './components/privateRoute' 
+import {PrivateRoute, RestrictedRoute} from './components/customRoute'  
 
 function App() { 
   return (
     <Router>
       <Switch>
-        <RestrictedRoute component={Login} path={Routes.login} exact />
-        <PrivateRoute component={Calendar} path={Routes.calendar} exact />
-        <PrivateRoute component={Reports} path={Routes.reports} exact />
-        <PrivateRoute component={Home} path={Routes.home} exact/>
+        <RestrictedRoute path={Routes.login} exact >
+          <Login />
+          </RestrictedRoute>
+        <PrivateRoute path={Routes.calendar} exact >
+          {withHeader(<Calendar />)}
+          </PrivateRoute>
+        <PrivateRoute path={Routes.reports} exact >
+          {withHeader(<Reports />)}
+          </PrivateRoute>
+        <PrivateRoute path={Routes.home} exact >
+          {withHeader(<Home />)}
+          </PrivateRoute>
       </Switch>
       {/*<Link to="/">Login</Link> / <Link to="/catalog">Catalog</Link>*/}
     </Router>
@@ -27,8 +34,8 @@ function withHeader(element: React.ReactElement ) : React.ReactElement{
     <div>
       <ul>
         <li><Link to={Routes.home}>Home</Link></li>
-        <li><Link to={Routes.calendar}>Calendar</Link></li>
-        <li><Link to={Routes.reports}>Reports</Link></li>
+        <li><Link to={Routes.calendar}>Kalender</Link></li>
+        <li><Link to={Routes.reports}>Berichte</Link></li>
       </ul>
       {element}
     </div>
