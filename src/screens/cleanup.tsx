8476a2f2cd7a0319ea01ba04773a38f7cf0c7315
Reto Grossenbacher <r.grossenbacher@screenimage.ch>
2021-08-26 21:19:35 +0200

    cleanup database

--- src/screens/cleanup.tsx
+++ src/screens/cleanup.tsx
@@ -2,38 +2,61 @@ import React, { useEffect, useState } from 'react';
 import ReactMapGL, { GeolocateControl, Marker, NavigationControl, ViewportProps } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { ReactComponent as MarkerIcon } from '../assets/mapbox-marker-icon-blue.svg';
-import firebase from 'firebase';
+import { authFirebase, database } from '../firebase/config';
+
+interface CleanupUser {
+  uid: string;
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+  email: string | null;
+}
 
 export default function Cleanup() {
   const [viewport, setViewport] = useState<ViewportProps>({
-    latitude: 47,
-    longitude: 8,
-    zoom: 14,
+    latitude: 46.82,
+    longitude: 8.26,
+    zoom: 7,
   });
-  const [myPosition, setMyPosition] = useState<{ lat: number; long: number }>();
-  const [members] = useState([
-    { name: 'Hans', lat: 47.163, long: 8.114 },
-    { name: 'Peter', lat: 47.164, long: 8.113 },
-    { name: 'Ueli', lat: 47.162, long: 8.116 },
-  ]);
+  const [myPosition, setMyPosition] = useState<GeolocationPosition>();
+  const [cleanupUser, setCleanupUser] = useState<CleanupUser[]>([]);
+  const [updated, setUpdated] = useState(0);
 
-  // useEffect(() => {
-  //   var db = firebase.database();
+  const loadCleanupUser = () => {
+    database
+      .ref(`cleanups/-eventid`)
+      .get()
+      .then((snapshot) => {
+        setCleanupUser(Object.values(snapshot.val()));
+      });
+  };
 
-  //   db.ref('users/regr').set({
-  //     username: 'regr',
-  //     name: 'Reto',
-  //   }).then();
-  // }, []);
+  useEffect(() => {
+    loadCleanupUser();
+    // momentan, alle 10s aktualisieren...
+    const interval = setInterval(loadCleanupUser, 10e3);
+    return () => clearInterval(interval);
+  }, []);
 
-  const handleViewportChange = (nextViewport: ViewportProps) => {
-    // console.log(nextViewport);
-    setViewport(nextViewport);
+  const handleViewport = (viewport: ViewportProps) => {
+    setViewport(viewport);
   };
 
   const handleGeolocate = (position: GeolocationPosition) => {
-    // console.log(position.coords);
-    setMyPosition({ lat: position.coords.latitude, long: position.coords.longitude });
+    setMyPosition(position);
+    // momentan, max. alle 10s updaten...
+    const user = authFirebase.currentUser;
+    if (user && updated < Date.now() - 10e3) {
+      setUpdated(Date.now());
+      const cleanupUser: CleanupUser = {
+        uid: user.uid,
+        email: user.email,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        timestamp: position.timestamp,
+      };
+      database.ref(`cleanups/-eventid/${user.uid}`).set(cleanupUser);
+    }
   };
 
   // https://visgl.github.io/react-map-gl/docs
@@ -44,7 +67,7 @@ export default function Cleanup() {
         mapboxApiAccessToken="pk.eyJ1IjoiZGVya3NlbnBoaWxpcHAiLCJhIjoiY2tycXV1ejZxMnFzNTJ1cnY5eHZ0ZXp1YSJ9.iWymYhi7VBjE_C6WIt0mOw"
         mapStyle="https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte.vt/style.json"
         {...viewport}
-        onViewportChange={handleViewportChange}
+        onViewportChange={handleViewport}
         width="100%"
         height="100vh"
       >
@@ -58,15 +81,17 @@ export default function Cleanup() {
           auto
         />
 
-        {members.map((m) => (
-          <Marker key={m.name} latitude={m.lat} longitude={m.long}>
-            <MarkerIcon style={{ opacity: 0.8, transform: 'translate(-50%, -50%)' }} />
-          </Marker>
-        ))}
+        {cleanupUser
+          .filter((u) => u.timestamp > Date.now() - 24 * 36e5)
+          .map((u) => (
+            <Marker key={u.uid} latitude={u.latitude} longitude={u.longitude}>
+              <MarkerIcon style={{ opacity: 0.8, transform: 'translate(-50%, -50%)' }} />
+            </Marker>
+          ))}
 
         {myPosition && (
           <div style={{ position: 'absolute', top: 10, right: 50 }}>
-            {myPosition.lat.toFixed(6)} / {myPosition.long.toFixed(6)}
+            {myPosition.coords.latitude.toFixed(6)} / {myPosition.coords.longitude.toFixed(6)}
           </div>
         )}
       </ReactMapGL>
