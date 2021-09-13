f0b2327d4922e9f40f7ab61a590e5ac8d883904c
Philipp Derksen <derksenphilipp@gmail.com>
2021-09-13 21:38:11 +0200

    Event card with flexbox

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import { Typography } from '@material-ui/core';
+import { Typography, Button, Box } from '@material-ui/core';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { Grid, Button } from '@material-ui/core';
 import { EventWithId } from '../common/firebase_types';
 import Map from '../components/map';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
@@ -15,12 +14,38 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      marginTop: '20px',
-    },
-    mapGridItem: {
       width: '100%',
+      marginTop: '20px',
+      marginBottom: '20px',
+    },
+    content: {
+      display: 'flex',
+      gap: '20px',
+      [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column',
+      },
+      [theme.breakpoints.up('md')]: {
+        flexDirection: 'row',
+      },
+    },
+    map: {
+      boxSizing: 'border-box',
       height: '350px',
-      padding: '1px',
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+      },
+      [theme.breakpoints.up('md')]: {
+        width: '50%',
+      },
+    },
+    info: {
+      boxSizing: 'border-box',
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+      },
+      [theme.breakpoints.up('md')]: {
+        width: '50%',
+      },
     },
     iconStart: { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -100%)' },
   })
@@ -37,28 +62,26 @@ function EventCard(props: EventCardProps) {
   return (
     <Card className={classes.root}>
       <CardHeader title={new Date(props.event.datum).toLocaleDateString('de-CH') + ' - ' + props.event.anlass} />
-      <CardContent style={{ width: '100%' }}>
-        <Grid container spacing={3}>
-          <Grid item sm={12} md={6} className={classes.mapGridItem}>
-            <Map initialPosition={props.event.position}>
-              <LocationOnOutlinedIcon fontSize="large" color="primary" className={classes.iconStart} />
-            </Map>
-          </Grid>
-          <Grid item sm={12} md={6}>
-            <Typography paragraph variant="h6">
-              Treffpunkt bei {props.event.ort} um {props.event.zeit} Uhr
-            </Typography>
-            <Button
-              color="secondary"
-              variant="contained"
-              endIcon={<KeyboardArrowRightIcon />}
-              disabled={!props.joinEnabled}
-              onClick={() => history.push(Routes.cleanup.replace(':id', props.event.id))}
-            >
-              Teilnehmen
-            </Button>
-          </Grid>
-        </Grid>
+      <CardContent className={classes.content}>
+        <Box className={classes.map}>
+          <Map initialPosition={props.event.position}>
+            <LocationOnOutlinedIcon fontSize="large" color="primary" className={classes.iconStart} />
+          </Map>
+        </Box>
+        <Box className={classes.info}>
+          <Typography paragraph variant="h6">
+            Treffpunkt bei {props.event.ort} um {props.event.zeit} Uhr
+          </Typography>
+          <Button
+            color="secondary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+            disabled={!props.joinEnabled}
+            onClick={() => history.push(Routes.cleanup.replace(':id', props.event.id))}
+          >
+            Teilnehmen
+          </Button>
+        </Box>
       </CardContent>
     </Card>
   );
