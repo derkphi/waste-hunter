eb82b554cb4b61d9e33ac504848d4674fa50d5ca
Christoph <christoph.wenger@students.bfh.ch>
2021-09-23 09:58:22 +0200

    Camera_test

--- src/components/camera/camera_component.tsx
+++ src/components/camera/camera_component.tsx
@@ -1,72 +1,66 @@
-import React, { useState,useRef,useCallback} from 'react';
-import {Button,} from '@material-ui/core';
-import Webcam from "react-webcam";
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import {projectStorage} from "../../firebase/config";
+import PhotoCameraRoundedIcon from "@material-ui/icons/PhotoCameraRounded";const useStyles = makeStyles((theme) => ({
+    root: {
+        height: "100%",
+        textAlign: 'center',
+    },
+    imgBox: {
+        maxWidth: "100%",
+        maxHeight: "100%",
+        margin: "10px"
+    },
+    img: {
+        height: "inherit",
+        maxWidth: "inherit",
+    },
+    input: {
+        display: "none"
+    }
+}));
 
-
-const useStyles = makeStyles((theme) => ({
-    camerabutton: { left: 20, bottom: 100, padding: 10},
-
+function Cameracomponent() {
+    const classes = useStyles();const [source, setSource] = useState("");
+    const handleCapture = (target:any) => {
+        if (target.files) {
+            if (target.files.length !== 0) {
+                const file = target.files[0];
+                const newUrl = URL.createObjectURL(file);
+                setSource(newUrl);
+            }
+        }
+    };return (
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <h5>Capture your image</h5>
+                    {source &&
+                    <Box display="flex" justifyContent="center" border={1} className={classes.imgBox}>
+                        <img src={source} alt={"snap"} className={classes.img}></img>
+                    </Box>}
+                    <input
+                        accept="image/*"
+                        className={classes.input}
+                        id="icon-button-file"
+                        type="file"
+                        capture="environment"
+                        onChange={(e) => handleCapture(e.target)}
+                    />
+                    <label htmlFor="icon-button-file">
+                        <IconButton
+                            color="primary"
+                            aria-label="upload picture"
+                            component="span"
+                        >
+                            <PhotoCameraRoundedIcon fontSize="large" color="primary" />
+                        </IconButton>
+                    </label>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
-));
-
-
-const videoConstraints = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    facingMode: "user"
-};
-
-export default function Cameracomponent() {
-
-    const classes = useStyles();
-    const [image,setImage]=useState('');
-    const webcamRef = useRef(null);
-    const capture = useCallback(
-        () => {
-            // @ts-ignore
-            const imageSrc = webcamRef.current.getScreenshot();
-            setImage(imageSrc);
-
-
-            projectStorage.ref(`/image/${image}`).put(imageSrc)
-            },
-
-                []
-        );
-
-   return (
-
-       <div className="webcam-container">
-           <div className="webcam-img">
-
-               {image == '' ? <Webcam
-                   audio={false}
-                   height={window.innerHeight}
-                   ref={webcamRef}
-                   screenshotFormat="image/jpeg"
-                   width={window.innerWidth}
-                   videoConstraints={videoConstraints}
-               /> : <img src={image} />}
-           </div>
-           <div className="ImageCam">
-
-               {image != '' ?
-                   <Button variant="contained"  color="secondary" size="small" onClick={(e) => {
-                       e.preventDefault();
-                       setImage('')
-                   }}
-                           className={classes.camerabutton}>
-                       Neues Bild</Button> :
-                   <Button variant="contained" color="secondary" size="small" onClick={(e) => {
-                       e.preventDefault();
-                       capture();
-                   }}
-                           className={classes.camerabutton}>Bild schiessen</Button>
-               }
-           </div>
-       </div>
-   );
-}
-
+export default Cameracomponent;
