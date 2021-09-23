b886d1a021e4be3c5d4500b514da694d303b8018
Reto Grossenbacher <r.grossenbacher@screenimage.ch>
2021-09-23 15:31:37 +0200

    mobile test

--- src/screens/cleanup.tsx
+++ src/screens/cleanup.tsx
@@ -30,17 +30,17 @@ import length from '@turf/length';
 // import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import { CameraAltRounded } from '@material-ui/icons';
+// import { CameraAltRounded } from '@material-ui/icons';
 import firebase from 'firebase/app';
 import DemoCleanups from '../components/demoCleanups';
 import SearchArea from '../components/maps/searchArea';
 import logo from '../assets/logo_transparent_background.png';
-import Cameracomponent from '../components/camera/camera_component';
-import SaveIcon from '@material-ui/icons/Save';
+// import Cameracomponent from '../components/camera/camera_component';
+// import SaveIcon from '@material-ui/icons/Save';
 import { apiAccessToken, mapStyle } from '../components/maps/config';
 
 const useStyles = makeStyles({
-  header: { position: 'absolute', left: 0, top: 0, width: '100%', padding: 10, textAlign: 'center' },
+  header: { position: 'absolute', left: '50%', top: 0, padding: 10, transform: 'translateX(-50%)' },
   logo: { marginTop: -7, height: 44, userDrag: 'none', userSelect: 'none' },
   main: {
     position: 'absolute',
@@ -104,7 +104,6 @@ export default function Cleanup() {
   const [lastPosition, setLastPosition] = useState<GeolocationPosition>();
   const [showPopup, setShowPopup] = useState('');
   const [showDialog, setShowDialog] = useState(false);
-  const [showCamera, setShowCamera] = useState(false);
   const [collected, setCollected] = useState<number>();
   const { id } = useParams<{ id: string }>();
   const user = authFirebase.currentUser;
@@ -169,184 +168,152 @@ export default function Cleanup() {
     history.push('/');
   };
 
-  if (!showCamera) {
-    return event ? (
-      <main className={classes.main} style={{ background: 'rgba(82, 135, 119, .1)' }}>
-        <ReactMapGL
-          mapboxApiAccessToken={apiAccessToken}
-          mapStyle={mapStyle}
-          {...viewport}
+  return event ? (
+    <main className={classes.main} style={{ background: 'rgba(82, 135, 119, .1)' }}>
+      <ReactMapGL
+        mapboxApiAccessToken={apiAccessToken}
+        mapStyle={mapStyle}
+        {...viewport}
+        onViewportChange={setViewport}
+        width="100%"
+        height="100%"
+        touchRotate
+      >
+        <NavigationControl style={{ left: 10, top: 10 }} onViewportChange={setViewport} />
+
+        <GeolocateControl
+          style={{ right: 10, top: 10 }}
+          positionOptions={{ enableHighAccuracy: true }}
+          onGeolocate={(p: GeolocationPosition) => setPosition(p)}
+          trackUserLocation={true}
           onViewportChange={setViewport}
-          width="100%"
-          height="100%"
-          touchRotate
-        >
-          <NavigationControl style={{ left: 10, top: 10 }} onViewportChange={setViewport} />
+          auto
+        />
 
-          <GeolocateControl
-            style={{ right: 10, top: 10 }}
-            positionOptions={{ enableHighAccuracy: true }}
-            onGeolocate={(p: GeolocationPosition) => setPosition(p)}
-            trackUserLocation={true}
-            onViewportChange={setViewport}
-            auto
-          />
+        {event.searchArea && <SearchArea data={event.searchArea} opacity={0.2} />}
 
-          {event.searchArea && <SearchArea data={event.searchArea} opacity={0.2} />}
+        {event && (
+          <Marker longitude={event.position.longitude} latitude={event.position.latitude}>
+            <LocationOnOutlinedIcon fontSize="large" color="primary" className={classes.iconStart} />
+          </Marker>
+        )}
 
-          {event && (
-            <Marker longitude={event.position.longitude} latitude={event.position.latitude}>
-              <LocationOnOutlinedIcon fontSize="large" color="primary" className={classes.iconStart} />
-            </Marker>
-          )}
-
-          {cleanupUsers?.map(
-            ({ uid, route, email }) =>
-              route && (
-                <div key={uid}>
-                  <Source id={`source-${uid}`} type="geojson" data={getGeoJsonLineFromRoute(route)} />
-                  <Layer
-                    id={`layer-${uid}`}
-                    type="line"
-                    source={`source-${uid}`}
-                    layout={{
-                      'line-cap': 'round',
-                      'line-join': 'round',
-                    }}
-                    paint={{
-                      'line-color': 'rgba(66, 100, 251, .4)',
-                      'line-width': 6,
-                    }}
-                  />
-                  {Object.values(route)
-                    .slice(-1)
-                    .map(([lng, lat, time]) => (
-                      <>
-                        <Marker
-                          key={time}
+        {cleanupUsers?.map(
+          ({ uid, route, email }) =>
+            route && (
+              <div key={uid}>
+                <Source id={`source-${uid}`} type="geojson" data={getGeoJsonLineFromRoute(route)} />
+                <Layer
+                  id={`layer-${uid}`}
+                  type="line"
+                  source={`source-${uid}`}
+                  layout={{
+                    'line-cap': 'round',
+                    'line-join': 'round',
+                  }}
+                  paint={{
+                    'line-color': 'rgba(66, 100, 251, .4)',
+                    'line-width': 6,
+                  }}
+                />
+                {Object.values(route)
+                  .slice(-1)
+                  .map(([lng, lat, time]) => (
+                    <>
+                      <Marker
+                        key={time}
+                        longitude={lng}
+                        latitude={lat}
+                        offsetTop={-32}
+                        offsetLeft={-17.5}
+                        className={`${classes.userMarker} ${time > Date.now() - 5 * 60e3 ? 'active' : 'inactive'}`}
+                      >
+                        <LocationOnIcon fontSize="large" className={classes.userIcon} />
+                        <div onClick={() => setShowPopup(showPopup === uid ? '' : uid)} className={classes.userCode}>
+                          {email?.replace(/\W+/g, '').slice(0, 2)}
+                        </div>
+                      </Marker>
+                      {showPopup === uid && (
+                        <Popup
                           longitude={lng}
                           latitude={lat}
-                          offsetTop={-32}
-                          offsetLeft={-17.5}
-                          className={`${classes.userMarker} ${time > Date.now() - 5 * 60e3 ? 'active' : 'inactive'}`}
+                          closeButton={false}
+                          closeOnClick={true}
+                          onClose={() => setShowPopup('')}
+                          anchor="bottom"
+                          offsetTop={-25}
+                          className={classes.userPopup}
                         >
-                          <LocationOnIcon fontSize="large" className={classes.userIcon} />
-                          <div onClick={() => setShowPopup(showPopup === uid ? '' : uid)} className={classes.userCode}>
-                            {email?.replace(/\W+/g, '').slice(0, 2)}
-                          </div>
-                        </Marker>
-                        {showPopup === uid && (
-                          <Popup
-                            longitude={lng}
-                            latitude={lat}
-                            closeButton={false}
-                            closeOnClick={true}
-                            onClose={() => setShowPopup('')}
-                            anchor="bottom"
-                            offsetTop={-25}
-                            className={classes.userPopup}
-                          >
-                            {email
-                              ?.replace(/@.*/g, '')
-                              .replace(/[._]+/g, ' ')
-                              .replace(/((^|\s)\S)/g, (m) => m.toUpperCase())}
-                          </Popup>
-                        )}
-                      </>
-                    ))}
-                </div>
-              )
-          )}
+                          {email
+                            ?.replace(/@.*/g, '')
+                            .replace(/[._]+/g, ' ')
+                            .replace(/((^|\s)\S)/g, (m) => m.toUpperCase())}
+                        </Popup>
+                      )}
+                    </>
+                  ))}
+              </div>
+            )
+        )}
 
-          {/* {position && (
+        {/* {position && (
           <div style={{ position: 'absolute', top: 10, right: 50 }}>
             {position.coords.longitude.toFixed(6)} / {position.coords.latitude.toFixed(6)} / {position.coords.accuracy}{' '}
             / {position.timestamp}
           </div>
         )} */}
-        </ReactMapGL>
+      </ReactMapGL>
 
-        <header className={classes.header}>
-          <img src={logo} alt="Waste Hunter" className={classes.logo} />
-        </header>
+      <header className={classes.header}>
+        <img src={logo} alt="Waste Hunter" className={classes.logo} />
+      </header>
 
-        <footer className={classes.footer}>
-          <Button
-            color="secondary"
-            variant="contained"
-            endIcon={<CameraAltRounded />}
-            onClick={() => {
-              setShowCamera(true);
-            }}
-          >
-            Markieren
+      <footer className={classes.footer}>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setCollected(undefined);
+            setShowDialog(true);
+          }}
+        >
+          Beenden
+        </Button>
+      </footer>
+
+      <Dialog open={showDialog} onClose={() => setShowDialog(false)} style={{ zIndex: 3000 }}>
+        <DialogTitle>Event beenden</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Vielen Dank für die Teilnahme beim "{event.anlass}" Event.</DialogContentText>
+          <DialogContentText>
+            Bitte geben sie zum beenden des Events noch die Menge ihres gesammelten Abfalles ein.
+          </DialogContentText>
+          <TextField
+            id="collected-waste"
+            label="Abfall (in Liter)"
+            type="number"
+            onChange={(e) => setCollected(Number(e.target.value))}
+            autoComplete="off"
+            required
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" onClick={() => setShowDialog(false)}>
+            Abbrechen
           </Button>
-
           <Button
-            color="primary"
             variant="contained"
-            onClick={() => {
-              setCollected(undefined);
-              setShowDialog(true);
-            }}
+            color="primary"
+            disabled={!(collected !== undefined && collected >= 0)}
+            onClick={handleDialogClose}
           >
             Beenden
           </Button>
-        </footer>
+        </DialogActions>
+      </Dialog>
 
-        <Dialog open={showDialog} onClose={() => setShowDialog(false)} style={{ zIndex: 3000 }}>
-          <DialogTitle>Event beenden</DialogTitle>
-          <DialogContent>
-            <DialogContentText>Vielen Dank für die Teilnahme beim "{event.anlass}" Event.</DialogContentText>
-            <DialogContentText>
-              Bitte geben sie zum beenden des Events noch die Menge ihres gesammelten Abfalles ein.
-            </DialogContentText>
-            <TextField
-              id="collected-waste"
-              label="Abfall (in Liter)"
-              type="number"
-              onChange={(e) => setCollected(Number(e.target.value))}
-              autoComplete="off"
-              required
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button color="secondary" onClick={() => setShowDialog(false)}>
-              Abbrechen
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={!(collected !== undefined && collected >= 0)}
-              onClick={handleDialogClose}
-            >
-              Beenden
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        <DemoCleanups className={classes.demoCleanups} event={event} />
-      </main>
-    ) : null;
-  } else {
-    return event ? (
-      <main className={classes.main} style={{ background: 'rgba(82, 135, 119, .1)' }}>
-        <Cameracomponent />
-
-        <footer className={classes.camera}>
-          <Button
-            color="primary"
-            variant="contained"
-            endIcon={<SaveIcon />}
-            onClick={() => {
-              setShowCamera(false);
-            }}
-          >
-            Speichern
-          </Button>
-        </footer>
-      </main>
-    ) : null;
-  }
+      <DemoCleanups className={classes.demoCleanups} event={event} />
+    </main>
+  ) : null;
 }
