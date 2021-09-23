7b1c3e09bc28fea480af1d703aa425462ca0562e
Philipp Derksen <derksenphilipp@gmail.com>
2021-09-23 11:32:52 +0200

    Refactored to statisticItem component.

--- src/components/statistic/statisticGroup.tsx
+++ src/components/statistic/statisticGroup.tsx
@@ -1,4 +1,5 @@
-import { Typography, makeStyles, Box } from '@material-ui/core';
+import { makeStyles, Box } from '@material-ui/core';
+import StatisticItem from './statisticItem';
 
 // red '#f6dfc6'
 
@@ -6,29 +7,6 @@ const useStyles = makeStyles({
   statsticContainer: {
     display: 'flex',
     flexWrap: 'wrap',
-  },
-  statisticItem: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    boxSizing: 'content-box',
-    width: '90px',
-    height: '90px',
-    backgroundColor: 'yellow',
-    padding: '5px',
-    marginTop: '5px',
-    marginBottom: '5px',
-    marginRight: '10px',
-    '&:last-child': {
-      marginRight: '0px',
-    },
-  },
-  statisticValue: {
-    fontSize: '2em',
-    textAlign: 'center',
-  },
-  statisticUnit: {
-    textAlign: 'right',
   },
 });
 
@@ -44,29 +22,10 @@ function StatisticGroup(props: StatisticGroupProps) {
 
   return (
     <Box className={classes.statsticContainer}>
-      {props.users && (
-        <Box className={classes.statisticItem} style={{ backgroundColor: '#d9b3d9' }}>
-          <Typography>Personen</Typography>
-          <Typography className={classes.statisticValue}>{props.users}</Typography>
-          <Typography className={classes.statisticUnit}>&nbsp;</Typography>
-        </Box>
-      )}
-      <Box className={classes.statisticItem} style={{ backgroundColor: '#f7eeb3' }}>
-        {' '}
-        <Typography>Suchzeit</Typography>
-        <Typography className={classes.statisticValue}>{Math.round(props.duration / 60e3)}</Typography>
-        <Typography className={classes.statisticUnit}>Min.</Typography>
-      </Box>
-      <Box className={classes.statisticItem} style={{ backgroundColor: '#bbdeeb' }}>
-        <Typography>Strecke</Typography>
-        <Typography className={classes.statisticValue}>{Math.round(props.distance * 1e3)}</Typography>
-        <Typography className={classes.statisticUnit}>Meter</Typography>
-      </Box>
-      <Box className={classes.statisticItem} style={{ backgroundColor: '#c8e9d4' }}>
-        <Typography>Abfall</Typography>
-        <Typography className={classes.statisticValue}>{Math.round(props.collected * 1e2) / 1e2}</Typography>
-        <Typography className={classes.statisticUnit}>Liter</Typography>
-      </Box>
+      {props.users && <StatisticItem label={'Personen'} value={props.users} color="#d9b3d9" />}
+      <StatisticItem label={'Suchzeit'} value={Math.round(props.duration / 60e3)} unit={'Min.'} color="#f7eeb3" />
+      <StatisticItem label={'Strecke'} value={Math.round(props.distance * 1e3)} unit={'Meter'} color="#bbdeeb" />
+      <StatisticItem label={'Abfall'} value={Math.round(props.collected * 1e2) / 1e2} unit={'Liter'} color="#c8e9d4" />
     </Box>
   );
 }
